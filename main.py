71e04958cc0fd42bcff523aa2f047e70ecdb1eec
julia <juliamscc@gmail.com>
2023-11-13 18:29:08 -0300

    Adicionando leitura de arquivo txt

--- main.py
+++ main.py
@@ -1,21 +1,30 @@
 from automato import *
 
+# Função para formatar os dados das transições:
+def formatar_transicoes(lista_de_transicoes):
+    transicoes = {}
+    for item in lista_de_transicoes:
+        lista = item.split('-')
+        tupla = tuple(lista[0].split())
+        transicoes[tupla]=lista[1]
+    return transicoes
+
 #Ler Arquivo
-with open('teste.txt', 'r') as arquivo:
+with open('gramaticas/teste.txt', 'r') as arquivo:
+    lista_dados = [linha.strip() for linha in arquivo]
 
-    linhas = arquivo.readlines()
+estados = lista_dados[0].split()
+estado_inicial = lista_dados[1]
+estados_finais = lista_dados[2].split()
+lista_de_transicoes = lista_dados[3:]
+transicoes = formatar_transicoes(lista_de_transicoes)
 
-linha_desejada = linhas[3]
-minha_variavel = linha_desejada.strip()
-print(minha_variavel)
+# print(estados)
+# print(estado_inicial)
+# print(estados_finais)
+# print(transicoes)
 
-# # Exemplo antigo
-# estados = {'A', 'B', 'C'}
-# estado_inicial = 'A'
-# estados_finais = {'C'}
-# transicoes = {('A', 'a'): 'B', ('B', 'b'): 'C'}
+automato = AutomatoFinito(estados, estado_inicial, estados_finais, transicoes)
 
-# automato = AutomatoFinito(estados, estado_inicial, estados_finais, transicoes)
-
-# palavra_teste = 'ab'
-# print(automato.reconhecer_palavra(palavra_teste))
+palavra_teste = 'ab'
+print(automato.reconhecer_palavra(palavra_teste))
