0b183ed1afa0253901f476524a95f2a568f3f7d6
Joao Vinicius <joviusdsg@gmail.com>
2023-11-13 00:24:18 -0300

    Estrutura de Leitura

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 from automato import *
 
-# Exemplo de uso:
-estados = {'A', 'B', 'C'}
-estado_inicial = 'A'
-estados_finais = {'C'}
-transicoes = {('A', 'a'): 'B', ('B', 'b'): 'C'}
-automato = AutomatoFinito(estados, estado_inicial, estados_finais, transicoes)
+#Ler Arquivo
+with open('teste.txt', 'r') as arquivo:
 
-palavra_teste = 'ab'
-print(automato.reconhecer_palavra(palavra_teste))  # Saída: True
+    linhas = arquivo.readlines()
+
+linha_desejada = linhas[3]
+minha_variavel = linha_desejada.strip()
+print(minha_variavel)
+
+# # Exemplo antigo
+# estados = {'A', 'B', 'C'}
+# estado_inicial = 'A'
+# estados_finais = {'C'}
+# transicoes = {('A', 'a'): 'B', ('B', 'b'): 'C'}
+
+# automato = AutomatoFinito(estados, estado_inicial, estados_finais, transicoes)
+
+# palavra_teste = 'ab'
+# print(automato.reconhecer_palavra(palavra_teste))
